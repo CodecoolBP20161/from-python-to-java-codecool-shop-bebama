7e743ee7e8b35d61c933497e2a1de644604ac347
cickib <bellakocsis06@gmail.com>
2016-11-15 13:18:38 +0100

    Universal filtering done

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -2,16 +2,24 @@
  * Created by cickib on 2016.11.14..
  */
 
-/* When the user clicks on the button,
- toggle between hiding and showing the dropdown content */
+function filter(filterby) {
+    var input, filter, ul, li, a;
 
-function filter() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("input-cat");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("search-cat");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
+    if (filterby === "cat") {
+        input = document.getElementById("input-cat");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("search-cat");
+        a = div.getElementsByTagName("a");
+
+    }
+    else if (filterby === "sup") {
+        input = document.getElementById("input-sup");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("search-sup");
+        a = div.getElementsByTagName("a");
+
+    }
+    for (var i = 0; i < a.length; i++) {
         if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
             a[i].style.display = "";
         } else {
