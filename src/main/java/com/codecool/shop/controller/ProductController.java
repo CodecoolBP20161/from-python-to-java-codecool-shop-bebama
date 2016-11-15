6bc8e708189768ce0bfc443c287bb0a262eee74b
Balazs Ando <elnesia@gmail.com>
2016-11-15 11:39:26 +0100

    Implement filtering

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -13,26 +14,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 
 public class ProductController {
 
     public static ModelAndView renderProducts(Request req, Response res) {
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+
 
         Map params = new HashMap<>();
         List<ProductCategory> allCategories = productCategoryDataStore.getAll();
         params.put("categories", allCategories);
+        List<com.codecool.shop.model.Supplier> allSuppliers = supplierDataStore.getAll();
+        params.put("suppliers", allSuppliers);
+        params.put("filterCategories", allCategories);
         return new ModelAndView(params, "product/index");
     }
 
     public static ModelAndView renderProductsByProductCategory(Request req, Response res) {
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+
 
         Map params = new HashMap<>();
         List<ProductCategory> category = new ArrayList<ProductCategory>();
         List<ProductCategory> allCategories = productCategoryDataStore.getAll();
         params.put("categories", allCategories);
+        List<com.codecool.shop.model.Supplier> allSuppliers = supplierDataStore.getAll();
+        params.put("suppliers", allSuppliers);
         category.add(productCategoryDataStore.find(Integer.parseInt(req.params(":id"))));
         params.put("filterCategories", category);
         return new ModelAndView(params, "product/index");
@@ -40,13 +49,17 @@ public class ProductController {
 
     public static ModelAndView renderProductsBySupplier(Request req, Response res) {
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+
 
         Map params = new HashMap<>();
         List<Supplier> supplier = new ArrayList<Supplier>();
+        List<ProductCategory> allCategories = productCategoryDataStore.getAll();
+        params.put("categories", allCategories);
         List<com.codecool.shop.model.Supplier> allSuppliers = supplierDataStore.getAll();
         params.put("suppliers", allSuppliers);
-        supplier.add((Supplier) supplierDataStore.find(Integer.parseInt(req.params(":id"))));
-        params.put("filterSuppliers", supplier);
+        supplier.add(supplierDataStore.find(Integer.parseInt(req.params(":id"))));
+        params.put("filterCategories", supplier);
         return new ModelAndView(params, "product/index");
     }
 
