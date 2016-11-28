bfa2105ec4d92fd7eaa0e783aec6e10705587a35
cickib <bellakocsis06@gmail.com>
2016-11-28 15:03:34 +0100

    constructor fixed

--- src/main/java/com/codecool/shop/model/ProductCategory.java
+++ src/main/java/com/codecool/shop/model/ProductCategory.java
@@ -6,7 +6,7 @@ public class ProductCategory extends BaseModel {
     private String department;
     private ArrayList<Product> products;
 
-    public ProductCategory(int id, String name, String department, String description) {
+    public ProductCategory(String name, String department, String description) {
         super(name);
         this.department = department;
         this.products = new ArrayList<>();
