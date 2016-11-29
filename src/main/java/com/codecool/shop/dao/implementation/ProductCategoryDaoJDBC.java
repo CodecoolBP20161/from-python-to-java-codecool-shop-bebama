514ea11fe8c0d313d5ca554d60b4bc83faf9abe1
cickib <bellakocsis06@gmail.com>
2016-11-29 10:28:55 +0100

    Correct execution of query

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBC.java
@@ -1,12 +1,9 @@
 package com.codecool.shop.dao.implementation;
-
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Created by makaimark on 2016.11.28..
  */
@@ -16,31 +13,32 @@ public class ProductCategoryDaoJDBC implements ProductCategoryDao {
     private static final String DB_PASSWORD = "postgres";
 
     private static ProductCategoryDaoJDBC instance = null;
-
     public static ProductCategoryDaoJDBC getInstance() {
         if (instance == null) {
             instance = new ProductCategoryDaoJDBC();
         }
         return instance;
     }
-
     @Override
     public void add(ProductCategory category) {
-        String query = "INSERT INTO category (id, name, description, department) " +
-                "VALUES ('" + category.getId() + "', '" + category.getName() + "', '" + category.getDescription() + "', '" + category.getDepartment() + "');";
-        executeQuery(query);
+        try(Connection connection = getConnection()) {
+            PreparedStatement query = connection.prepareStatement("INSERT INTO category (name, department, description) VALUES (?, ?, ?);");
+            query.setString(1, category.getName());
+            query.setString(2, category.getDepartment());
+            query.setString(3, category.getDescription());
+            query.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-
     @Override
     public ProductCategory find(int id) {
-
         String query = "SELECT * FROM category WHERE id ='" + id + "';";
-
         try (Connection connection = getConnection();
-             Statement statement = connection.createStatement();
+             Statement statement =connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query);
-        ) {
-            if (resultSet.next()) {
+        ){
+            if (resultSet.next()){
                 ProductCategory result = new ProductCategory(
                         resultSet.getString("name"),
                         resultSet.getString("description"),
@@ -54,25 +52,22 @@ public class ProductCategoryDaoJDBC implements ProductCategoryDao {
         }
         return null;
     }
-
     @Override
     public void remove(int id) {
-        String query = "DELETE FROM category WHERE id = '" + id + "';";
+        String query = "DELETE FROM category WHERE id = '" + id +"';";
         executeQuery(query);
     }
-
     @Override
     public List<ProductCategory> getAll() {
         String query = "SELECT * FROM category;";
-
         List<ProductCategory> resultList = new ArrayList<>();
-
         try (Connection connection = getConnection();
-             Statement statement = connection.createStatement();
+             Statement statement =connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query);
-        ) {
-            while (resultSet.next()) {
-                ProductCategory category = new ProductCategory(resultSet.getString("name"),
+        ){
+            while (resultSet.next()){
+                ProductCategory category = new ProductCategory(
+                        resultSet.getString("name"),
                         resultSet.getString("description"),
                         resultSet.getString("department"));
                 resultList.add(category);
@@ -82,20 +77,17 @@ public class ProductCategoryDaoJDBC implements ProductCategoryDao {
         }
         return resultList;
     }
-
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
                 DBURL,
                 DB_USER,
                 DB_PASSWORD);
     }
-
     private void executeQuery(String query) {
         try (Connection connection = getConnection();
-             Statement statement = connection.createStatement();
-        ) {
+             Statement statement =connection.createStatement();
+        ){
             statement.execute(query);
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
