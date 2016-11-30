643ae5c64082ba0e2196c0945eca47f1bff8d781
makaimark <makaimark@gmail.com>
2016-11-30 14:06:55 +0100

    i can read info from config

--- src/main/java/com/codecool/shop/dao/implementation/jdbc/AbstractDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/jdbc/AbstractDaoJDBC.java
@@ -1,8 +1,12 @@
 package com.codecool.shop.dao.implementation.jdbc;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class AbstractDaoJDBC {
 
@@ -12,5 +16,15 @@ public class AbstractDaoJDBC {
 
     protected Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DBURL, DB_USER, DB_PASSWORD);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Properties pro = new Properties();
+        FileInputStream in = new FileInputStream("/home/makaimark/Desktop/Codecool/JAVA/from-python-to-java-codecool-shop-bebama/src/main/resources/public/config/config.properties");
+        pro.load(in);
+// getting values from property file
+        System.out.println(pro.getProperty("DBURL"));
+        System.out.println(pro.getProperty("DB_USER"));
+        System.out.println(pro.getProperty("DB_PASSWORD"));
     }
 }
