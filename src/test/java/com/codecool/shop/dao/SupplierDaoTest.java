6b9d56a4b4d31ab504f39bb34fbfb0c7406d3c43
cickib <bellakocsis06@gmail.com>
2016-11-30 10:34:15 +0100

    junit dependency and skeleton tests added

---
+++ src/test/java/com/codecool/shop/dao/SupplierDaoTest.java
@@ -0,0 +1,43 @@
+package com.codecool.shop.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cickib on 2016.11.30..
+ */
+public class SupplierDaoTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void find() throws Exception {
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+}
