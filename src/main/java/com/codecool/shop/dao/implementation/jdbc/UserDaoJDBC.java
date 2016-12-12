25127492170c761f695d05ea0ef4cdd6c3703516
Balazs Ando <elnesia@gmail.com>
2016-12-12 14:30:45 +0100

    Implement DaoJDBC for user class

---
+++ src/main/java/com/codecool/shop/dao/implementation/jdbc/UserDaoJDBC.java
@@ -0,0 +1,79 @@
+package com.codecool.shop.dao.implementation.jdbc;
+
+import com.codecool.shop.cart.User;
+import com.codecool.shop.dao.UserDao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoJDBC extends AbstractDaoJDBC implements UserDao{
+    private static ProductCategoryDaoJDBC instance = null;
+
+    public static ProductCategoryDaoJDBC getInstance() {
+        if (instance == null) {
+            instance = new ProductCategoryDaoJDBC();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(User user) {
+        try (Connection connection = getConnection()) {
+            PreparedStatement query = connection.prepareStatement("INSERT INTO category (name, email, password, welcomeEmail) VALUES (?, ?, ?, ?);");
+            query.setString(1, user.getName());
+            query.setString(2, user.getEmail());
+            query.setString(3, user.getPassword());
+            query.setBoolean(4, user.getWelcomeEmail());
+            query.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public User find(int id) {
+        User result = null;
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery("SELECT * FROM user WHERE id ='" + id + "';");
+        ) {
+            if (rs.next()) {
+                result = new User(rs.getString("name"), rs.getString("email"), rs.getString("password"), rs.getBoolean("welcomeEmail"));
+                result.setId(id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public void remove(int id) {
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+        ) {
+            statement.execute("DELETE FROM user WHERE id = '" + id + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        List<User> resultList = new ArrayList<>();
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery("SELECT * FROM user;");
+        ) {
+            while (rs.next()) {
+                User user = new User(rs.getString("name"), rs.getString("email"), rs.getString("password"), rs.getBoolean("welcomeEmail"));
+                user.setId(rs.getInt("id"));
+                resultList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultList;
+    }
+}
