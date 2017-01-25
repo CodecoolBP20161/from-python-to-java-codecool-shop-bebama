5ba4afa136ef08035b61956c0525d4f299f9e1d4
cickib <bellakocsis06@gmail.com>
2017-01-25 16:12:07 +0100

    UserController and PaymentController methods seperated

--- src/main/java/com/codecool/shop/controller/UserController.java
+++ src/main/java/com/codecool/shop/controller/UserController.java
@@ -3,69 +3,40 @@ package com.codecool.shop.controller;
 import com.codecool.shop.HashClass;
 import com.codecool.shop.cart.User;
 import com.codecool.shop.cart.implementation.Order;
-import com.codecool.shop.dao.implementation.jdbc.ProductCategoryDaoJDBC;
-import com.codecool.shop.dao.implementation.jdbc.ProductDaoJDBC;
-import com.codecool.shop.dao.implementation.jdbc.SupplierDaoJDBC;
 import com.codecool.shop.dao.implementation.jdbc.UserDaoJDBC;
 import com.codecool.shop.email.service.EmailSenderService;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-
-/**
- * Created by cickib on 2016.12.13..
- */
-
 public class UserController extends AbstractController {
     private static UserDaoJDBC users = UserDaoJDBC.getInstance();
 
-    public static boolean isLoggedIn(Request request) {
-        if (request.session().attribute("isLoggedIn") == null) {
-            request.session().attribute("isLoggedIn", false);
-            params.put("isLoggedIn", false);
+    private static void switchLoginData(Request req, boolean bool) {
+        req.session().attribute("isLoggedIn", bool);
+        params.put("isLoggedIn", bool);
+    }
+
+    public static boolean isLoggedIn(Request req) {
+        if (req.session().attribute("isLoggedIn") == null) {
+            switchLoginData(req, false);
         }
-        return request.session().attribute("isLoggedIn");
+        return req.session().attribute("isLoggedIn");
     }
 
     public static ModelAndView renderForm(Request req, Response res) {
-        params.put("categories", ProductCategoryDaoJDBC.getInstance().getAll());
-        params.put("suppliers", SupplierDaoJDBC.getInstance().getAll());
         params.put("order", Order.getOrder(req));
-        params.put("products", ProductDaoJDBC.getInstance().getAll());
         return new ModelAndView(params, "user/signup_form");
-    }
-
-    private static void signUpLogic(Request req, Response res) {
-        User newUser = new User(req.queryParams("name"), req.queryParams("email"), req.queryParams("pwd"), false);
-        params.put("existingEmail", false);
-        params.remove("name");
-        params.remove("email");
-        try {
-            users.add(newUser);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String recipient = req.queryParams("email");
-        String recipientName = req.queryParams("name");
-        try {
-            EmailController.builder("bebamashop@gmail.com", recipient, "Welcome email", recipientName, EmailSenderService.formatWelcomeEmail(recipientName));
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        res.redirect("/successful_registration");
     }
 
     public static ModelAndView getFormData(Request req, Response res) throws Exception {
         if (users.find(req.queryParams("name")) == null) {
             params.remove("existingName");
             if (users.findEmail(req.queryParams("email")) == null) {
-                signUpLogic(req, res);
+                params.put("existingEmail", false);
+                params.remove("name");
+                params.remove("email");
+                signUpUser(req, res);
             } else {
                 params.put("name", req.queryParams("name"));
                 params.put("email", req.queryParams("email"));
@@ -81,15 +52,26 @@ public class UserController extends AbstractController {
         return new ModelAndView(params, "product/index");
     }
 
-    public static ModelAndView success(Request req, Response res) {
+    private static void signUpUser(Request req, Response res) {
+        User newUser = new User(req.queryParams("name"), req.queryParams("email"), req.queryParams("pwd"), false);
+        try {
+            users.add(newUser);
+        } catch (Exception e) {
+            System.out.println("Failed to add new user.\n" + e.getMessage());
+            e.printStackTrace();
+        }
+        PaymentController.sendSignUpEmail(req);
+        res.redirect("/successful_registration");
+    }
+
+    public static ModelAndView successfulRegistration(Request req, Response res) {
         return new ModelAndView(params, "user/successful_registration");
     }
 
-    public static ModelAndView login(Request req, Response res) throws Exception {
+    public static Response login(Request req, Response res) throws Exception {
         boolean pwdMatch = HashClass.checkPassword(req.queryParams("login-name"), req.queryParams("login-pwd"));
         if (pwdMatch) {
-            req.session().attribute("isLoggedIn", true);
-            params.put("isLoggedIn", isLoggedIn(req));
+            switchLoginData(req, true);
             req.session().removeAttribute("failedLogin");
             params.remove("failedLogin");
             res.redirect("/");
@@ -98,13 +80,13 @@ public class UserController extends AbstractController {
             req.session().attribute("failedLogin", true);
         }
         res.redirect("/");
-        return ProductController.renderProducts(req, res);
+        return res;
     }
 
-    public static ModelAndView logout(Request req, Response res) throws Exception {
-        req.session().attribute("isLoggedIn", false);
-        params.put("isLoggedIn", false);
+    public static Response logout(Request req, Response res) throws Exception {
+        switchLoginData(req, false);
         Order.dropOrder(req);
-        return ProductController.renderProducts(req, res);
+        res.redirect("/");
+        return res;
     }
 }
